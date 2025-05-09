b8d7b21161c429c44050333a5349692410cd6b80
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-05-09 01:11:48 +0100

    GetHashCode and Equals done

--- EqualPlayer/Player.cs
+++ EqualPlayer/Player.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 public class Player
 {
 public PlayerClass PClass {get; }
@@ -6,5 +8,29 @@ public Player (PlayerClass pClass, string name )
 {
 PClass = pClass ;
 Name = name ;
+}
+public override int GetHashCode()
+{
+    return PClass.GetHashCode() ^ Name.GetHashCode();
+}
+
+public override bool Equals(object other)
+{
+    bool equal_value;
+    Player otherPlayer = other as Player;
+    if(otherPlayer is null) 
+    {
+    equal_value = false;
+    }
+    else if(PClass == otherPlayer.PClass && Name == otherPlayer.Name)
+    { 
+    equal_value = true;
+    }
+    else
+    {
+    return false;
+    }
+    return equal_value;
+
 }
 }
