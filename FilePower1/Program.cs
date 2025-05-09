28ae0456bcdd6d269410afd98981d19e47ef8352
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-05-09 01:38:31 +0100

    while statment finished

--- FilePower1/Program.cs
+++ FilePower1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace FilePower1
 {
@@ -11,6 +12,21 @@ namespace FilePower1
             string input_string;
             bool input_loop = true;
             Queue<string> str_Queue = new Queue<string>();
+
+             while(input_loop == true)
+            {
+                input_string = Console.ReadLine();
+
+                if(input_string == "")
+                {
+                    input_loop = false;
+                }
+                else
+                {
+                    str_Queue.Enqueue(input_string);
+                }
+            }
+            File.WriteAllLines(input_path, str_Queue);
         }
     }
 }
