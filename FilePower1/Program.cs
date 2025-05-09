e15f98f52a9e44a4303c161f6224a8d510ee98f9
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-05-09 01:33:06 +0100

    Queue setup

---
+++ FilePower1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FilePower1
+{
+    public class Program
+    {
+       private static void Main(string[] args)
+        {
+            string input_path = args[0];
+            string input_string;
+            bool input_loop = true;
+            Queue<string> str_Queue = new Queue<string>();
+        }
+    }
+}
